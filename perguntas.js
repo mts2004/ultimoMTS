d57739ab5f4cdc410bdd97631ece722a76d92ca0
mts2004 <00001098144922sp@al.educacao.sp.gov.br>
2024-11-26 14:36:41 -0300

    VD

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'futebol',
+    'O que é Abel Ferreira?',
+    'Treinador e ex-jogador de futebol português'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'sobre mim',
+    'quem sou eu?',
+    'MATEUS'
+)
